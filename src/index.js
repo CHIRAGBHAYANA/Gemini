60951d05e4c4a8721827dc0ff3a156c24f35cc39
Chirag Bhayana <chiragbhayana2k@gmail.com>
2026-02-22 00:19:25 +0530

    DEMO-2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const add = require('./methods/add');
+const multiply = require('./methods/multiply');
 const subtract = require('./methods/subtract');
 
 console.log(add(2,2))
 console.log(subtract(4,2));
+console.log(multiply(2,3))
--- src/methods/subtract.js
+++ src/methods/subtract.js
@@ -1,5 +1,5 @@
 function subtract(a,b) {
-  return a + b;
+  return a - b;
 }
 
 export default subtract;
